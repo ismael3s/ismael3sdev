5e49e7f616752e41ae773298daed5569b1372be3
Ismael Santana <souz4ismael@gmail.com>
2024-08-29 06:53:23 -0300

    feat: integrate with sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import expressiveCode from "astro-expressive-code";
+import sentry from "@sentry/astro";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://ismael3s.dev',
-  integrations: [expressiveCode(),mdx(), sitemap(), tailwind(), ]
+  site: "https://ismael3s.dev",
+  integrations: [
+    expressiveCode(),
+    mdx(),
+    sitemap(),
+    tailwind(),
+    sentry({
+      dsn: "https://d37e68d13e23d587da1bfa1edba1311e@o4507820477186048.ingest.us.sentry.io/4507860001488896",
+      sourceMapsUploadOptions: {
+        project: "ismael3sdev",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
 });
