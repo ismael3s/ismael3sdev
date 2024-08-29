8c4f32e6b761d5b61891c4a9d7fedf676fcd990c
Ismael Santana <souz4ismael@gmail.com>
2024-08-29 07:46:19 -0300

    chore: comment astro config for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import expressiveCode from "astro-expressive-code";
-import sentry from "@sentry/astro";
+// import sentry from "@sentry/astro";
 
 export default defineConfig({
   site: "https://ismael3s.dev",
@@ -12,12 +12,14 @@ export default defineConfig({
     mdx(),
     sitemap(),
     tailwind(),
-    sentry({
-      dsn: "https://d37e68d13e23d587da1bfa1edba1311e@o4507820477186048.ingest.us.sentry.io/4507860001488896",
-      sourceMapsUploadOptions: {
-        project: "ismael3sdev",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
-    }),
+    // sentry({
+    //   dsn: "https://d37e68d13e23d587da1bfa1edba1311e@o4507820477186048.ingest.us.sentry.io/4507860001488896",
+    //   sourceMapsUploadOptions: {
+    //     project: "ismael3sdev",
+    //     authToken:
+    //       process.env.SENTRY_AUTH_TOKEN ||
+    //       "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",
+    //   },
+    // }),
   ],
 });
